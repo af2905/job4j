9de732b777f06232dbd734b929ae4f8cbabb9ee6
af2905 <af2905g@gmail.com>
2019-11-19 22:16:52 +0300

    Prime numbers. [# 200377] code refactoring

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,18 +3,13 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
-        boolean isPrime;
+
+        CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
         for (int i = 2; i <= finish; i++) {
-            isPrime = true;
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    isPrime = false;
-                }
-            }
+            boolean isPrime = checkPrimeNumber.check( i );
             if (isPrime) count++;
         }
         return count;
     }
 }
-
 
