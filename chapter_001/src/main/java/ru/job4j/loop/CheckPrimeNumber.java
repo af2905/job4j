515ff9871bfe143c981bb37c88360b2bda0c3666
af2905 <af2905g@gmail.com>
2019-11-19 21:02:21 +0300

    Prime number. [# 171694] changes to the code

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,12 +2,17 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public boolean check(int num) {
+        boolean isPrime = true;
+
+        if (num == 0 || num == 1) {
+            isPrime = false;
+        }
 
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
-                return false;
+                isPrime = false;
             }
         }
-        return true;
+        return isPrime;
     }
 }
