e051f51d79acc54ff9c27401e4d0f91995b9ae7d
af2905 <af2905g@gmail.com>
2019-11-19 15:04:09 +0300

    Is there a triangle? [# 200124] simplified

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -2,13 +2,8 @@ package ru.job4j.condition;
 
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
-        boolean isExist = false;
 
-        if (ab + ac > bc && ac + bc > ab && ab + bc > ac) {
-            isExist = true;
-        }
-
-        return isExist;
+        return (ab + ac > bc && ac + bc > ab && ab + bc > ac);
     }
 }
 
