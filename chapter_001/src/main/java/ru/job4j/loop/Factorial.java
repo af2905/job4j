237452ef6e2d84614f3a3a81b1931512ba9ac660
af2905 <af2905g@gmail.com>
2019-11-19 15:23:45 +0300

    Create a program that calculates factorial. [# 193] fixed

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -2,13 +2,10 @@ package ru.job4j.loop;
 
 public class Factorial {
     public int calc(int n) {
-        int result = 0;
+        int result = 1;
 
-        for (int i = 0; i <= n; i++) {
-            if (result == 0) {
-                result = 1;
-                continue;
-            } else result *= i;
+        for (int i = 1; i <= n; i++) {
+             result *= i;
         }
         return result;
     }
