6a459669da793f04afcc4b4f75c1fcf71bd87970
af2905 <af2905g@gmail.com>
2019-11-20 10:49:30 +0300

    JavaDoc. Code convention [#4087]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,15 @@
+package ru.job4j.calculate;
+
+/**
+ * @author Anatolii Frolov
+ * @version 1.0
+ * @since 1.0
+ */
+public class Calculate {
+    /**
+     * @param args - main method (program entry point)
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Anatolii Frolov
+ * @version 1.0
+ * @since 1.0
+ */
+package ru.job4j.calculate;
