1b3f0c7c4a7d2df098f1f78e9bf2b42b6cff38c8
af2905 <af2905g@gmail.com>
2019-11-20 21:03:01 +0300

    The word ends with ... [# 156317]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        int wordIndex = word.length - 1;
+
+        for (int postIndex = post.length - 1; postIndex >= 0; postIndex--) {
+            if (!(post[postIndex] == word[wordIndex])) {
+                result = false;
+                break;
+            } else result = true;
+            wordIndex--;
+        }
+
+        return result;
+    }
+}
