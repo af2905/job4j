4769b36ae1bf3d38160d87150839718067e42632
af2905 <af2905g@gmail.com>
2019-11-22 19:09:30 +0300

    Sort by selection. [# 201259] refactoring

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -2,22 +2,19 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        int min = MinDiapason.findMin( data, 0, data.length );
-        int index = FindLoop.indexOf( data, min, 0, data.length );
 
-        int tmp = data[0];
-        data[0] = data[index];
-        data[index] = tmp;
+        for (int out = 0; out < data.length; out++) {
+            for (int inner = 0; inner < data.length; inner++) {
 
-        for (int out = 0; out < data.length - 1; out++) {
-            for (int inner = 1; inner < data.length - 1; inner++) {
-                if (data[inner] > data[inner + 1]) {
-                    int temp = data[inner];
-                    data[inner] = data[inner + 1];
-                    data[inner + 1] = temp;
-                }
+                int min = MinDiapason.findMin( data, out, data.length );
+                int index = FindLoop.indexOf( data, min, out, data.length );
+
+                int tmp = data[out];
+                data[out] = data[index];
+                data[index] = tmp;
             }
         }
         return data;
     }
 }
+
